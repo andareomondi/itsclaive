9a569e3621b6f3dc672db3b1eb68ff5c17d99e46
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-10-13 11:17:17 +0300

    creating the view

--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+class Home(View):
+    def get(self, request):
+        return render(request, template_name='base/home.html')
+
+class About(View):
+    def get(self, request):
+        return render(request, template_name='base/about.html')
--- itsclaive/urls.py
+++ itsclaive/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from base.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='home'),
+    path('about/', About.as_view(), name='about'),
 ]
