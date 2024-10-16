d60c2f3036987a396afc3c124a069039975193f1
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-10-16 17:36:34 +0300

    changes on the url and etc

--- itsclaive/settings.py
+++ itsclaive/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d)z3^qoxjd&w3e0533*iktfeh(pwxy9k)#u4-)*nsifqh@=qyh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
