b220be152a97d3681482673764f7142c65eea915
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-10-17 21:10:12 +0300

    cooking complete

--- member/views.py
+++ member/views.py
@@ -4,11 +4,23 @@ from django.views import View
 class Home(View):
     def get(self, request):
         return render(request, 'member/index.html')
+    def post(self, request):
+        name = request.POST.get('bookingname')
+        print(name)
+        return render(request, 'member/index.html')
     
 class Services(View):
     def get(self, request):
         return render(request, 'member/services.html')
+    def post(self, request):
+        name = request.POST.get('bookingname')
+        print(name)
+        return render(request, 'member/services.html')
     
 class Portfolio(View):
     def get(self, request):
+        return render(request, 'member/portfolio.html')
+    def post(self, request):
+        name = request.POST.get('bookingname')
+        print(name)
         return render(request, 'member/portfolio.html')
