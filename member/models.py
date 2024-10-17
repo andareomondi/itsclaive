e607ed86bfb96030a21b47e82c00e0b8053628d4
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-10-17 23:50:05 +0300

    almost there

--- member/models.py
+++ member/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Session(models.Model):
+    name  = models.CharField(max_length=255)
+    venue = models.CharField(max_length=255)
+    date = models.DateField()
+    phone_number = models.IntegerField()
+    service_type = models.CharField(max_length=255)
+    def  __str__(self):
+        return f'session {self.id} by {self.name} at {self.venue} on {self.date}'
+
+
--- member/views.py
+++ member/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib import messages
+from .models import Session
 # Create your views here.
 class Home(View):
     def get(self, request):
         return render(request, 'member/index.html')
     def post(self, request):
         name = request.POST.get('bookingname')
-        print(name)
+        number = request.POST.get('number')
+        location = request.POST.get('location')
+        date = request.POST.get('date')
+        service = request.POST.get('service')
+        session = Session()
+        session.name = name
+        session.venue =location
+        session.date = date
+        session.phone_number = number
+        session.service_type = service
+        session.save()
+        messages.success(request, 'Your request  has been sent successfully')
+
         return render(request, 'member/index.html')
     
 class Services(View):
@@ -14,7 +28,19 @@ class Services(View):
         return render(request, 'member/services.html')
     def post(self, request):
         name = request.POST.get('bookingname')
-        print(name)
+        number = request.POST.get('number')
+        location = request.POST.get('location')
+        date = request.POST.get('date')
+        service = request.POST.get('service')
+        session = Session()
+        session.name = name
+        session.venue =location
+        session.date = date
+        session.phone_number = number
+        session.service_type = service
+        session.save()
+        messages.success(request, 'Your request  has been sent successfully')
+
         return render(request, 'member/services.html')
     
 class Portfolio(View):
@@ -22,5 +48,17 @@ class Portfolio(View):
         return render(request, 'member/portfolio.html')
     def post(self, request):
         name = request.POST.get('bookingname')
-        print(name)
+        number = request.POST.get('number')
+        location = request.POST.get('location')
+        date = request.POST.get('date')
+        service = request.POST.get('service')
+        session = Session()
+        session.name = name
+        session.venue =location
+        session.date = date
+        session.phone_number = number
+        session.service_type = service
+        session.save()
+        messages.success(request, 'Your request  has been sent successfully')
+
         return render(request, 'member/portfolio.html')
